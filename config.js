0f0df85030a8a464640b19d28e53e67b45b399e1
nathanib <119151236+nathanib@users.noreply.github.com>
2023-09-28 03:13:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['+14432187210', 'me', true],
+  ['+918438374975', 'Ash', true], 
   [''] 
 ] //Number of owners
 
